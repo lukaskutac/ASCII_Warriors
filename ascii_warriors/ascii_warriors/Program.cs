7c959da6d0ced2a1799d293c34da8c3a003db7bd
kutil <81024726+lukaskutac@users.noreply.github.com>
2023-02-24 23:02:37 +0100

    structure

---
+++ ascii_warriors/ascii_warriors/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ascii_warriors
+{
+    internal class Program
+    {
+        abstract class warrior
+        {
+            public int health;
+            public int damage;
+
+            public abstract void attack();
+        }
+
+        abstract class melee : warrior
+        {
+            public abstract void walk();
+        }
+
+        abstract class ranged : warrior
+        {
+            
+        }
+
+        abstract class knight : melee
+        {
+            
+        }
+
+        abstract class golem : melee
+        {
+            
+        }
+
+        abstract class mage : ranged
+        {
+            
+        }
+
+        abstract class ninja : ranged
+        {
+            
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
